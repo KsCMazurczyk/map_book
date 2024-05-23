27eb1b0a222e6a6565dc61feeacf9e3848c9209f
Bartek <87177145+KsCMazurczyk@users.noreply.github.com>
2024-05-23 12:15:14 +0200

    wip: add read function

--- main.py
+++ main.py
@@ -8,12 +8,32 @@ data_of_users: list = [
     {'name': 'Seba', 'Surname': 'Dudek', 'posts': 10, 'location': 'Siedlce'},
 ]
 print(f'Witaj {data_of_users[0]['name']}')
-def read(users: list)->None:
+
+
+def read(users: list) -> None:
     """
-    this is a function to show users from an list
+    show users from an list
     :param users: a list of users
     :return: None
     """
     for user in users[1:]:
         print(f'Twój Znajomy: {user['name']}, opublikował: {user["posts"]} postów')
+
+
+# read(data_of_users)
+def add_user(users: list) -> None:
+    """
+    add a user to a users list
+    :param users: user list
+    :return: None
+    """
+    name: str = input("Enter your name:")
+    Surname: str = input("Enter your Surnname:")
+    posts: int = int(input("Enter your posts number:"))
+    location: str = input("Enter your location:")
+    new_user: dict = {'name': name, 'Surname': Surname, 'posts': posts, 'location': location}
+    users.append(new_user)
+
+
+add_user(data_of_users)
 read(data_of_users)
